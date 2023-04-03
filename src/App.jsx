b1e3d5bb1db67e302d285438d562e9dd059cbcc3
Earl A. Villapaz <jasonburning16@gmail.com>
2023-04-03 22:38:32 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Contents from './components/Contents';
+const App = () => {
+  return (
+    <div>
+      <Contents />
+    </div>
+  );
+};
+
+export default App;
